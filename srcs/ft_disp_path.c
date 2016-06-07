66730824ba821b2f84e195fb00af9aab8f7f7467
cledant <cledant@student.42.fr>
2016-06-07 19:04:22 +0200

    meh WIP affichage

--- srcs/ft_disp_path.c
+++ srcs/ft_disp_path.c
@@ -6,7 +6,7 @@
 /*   By: cledant <cledant@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/07 12:46:29 by cledant           #+#    #+#             */
-/*   Updated: 2016/06/07 12:52:35 by cledant          ###   ########.fr       */
+/*   Updated: 2016/06/07 19:03:56 by cledant          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,31 @@
 
 void	ft_disp_path(t_env *env)
 {
-	char	**split;
+	char		**split;
+	long int	*tab;
+	size_t		i;
+	size_t		j;
+	size_t		one;
+	size_t		size;
 
+	i = 0;
+	size = ft_part_nb(env->path, ' ');
 	if ((split = ft_strsplit(env->path, ' ')) == NULL)
 		return ;
-
+	if ((tab = (long int *)malloc(sizeof(long int) * size)) == NULL)
+	{
+		ft_clean(split, tab);
+		return ;
+	}
+	while (i < size)
+	{
+		tab[i][0] = 1 - i;
+		tab[i][1] = 0;
+		i++;
+	}
+	i = 0;
+	while (i < size)
+	{
+		
+	}
 }
